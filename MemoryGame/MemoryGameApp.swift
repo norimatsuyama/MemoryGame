29c25b2558c100ba0d8eb5b2c18481eed8312c81
Norika Matsuyama <norika.sf@gmail.com>
2023-10-22 16:29:09 -0700

    Initial Commit

---
+++ MemoryGame/MemoryGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryGameApp.swift
+//  MemoryGame
+//
+//  Created by Norika on 10/22/23.
+//
+
+import SwiftUI
+
+@main
+struct MemoryGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
